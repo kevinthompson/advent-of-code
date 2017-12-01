77a7e49eebd776e890feec9019f516bccabfa12f
Kevin Thompson <kevin@kevinthompson.info>
2017-12-01 09:37:05 -0800

    Dynamically Require All Solution Files

--- 2017/01-inverse-captcha/results.rb
+++ 2017/01-inverse-captcha/results.rb
@@ -1,6 +1,4 @@
-require_relative 'solution/part_one'
-require_relative 'solution/part_two'
-
+Dir[File.expand_path(File.dirname(__FILE__)) + '/solution/*.rb'].each { |file| require file }
 input = File.open('input.txt').read
 solution = Solution.new(input)
 solution.results.each { |result| print "#{result}\n" }
