0192bc4f2cca4714bdaae9a89d4620c9cfc67d59
Kevin Thompson <email@kevinthompson.info>
2024-12-01 22:22:46 -0800

    2024 day 2

---
+++ 2024/02/part2.rb
@@ -0,0 +1,20 @@
+File.read(File.join(__dir__, 'input'))
+    .split("\n")
+    .map(&:split)
+    .count do |group|
+      (0..group.size).any? do |j|
+        report = group.dup
+        report.delete_at(j)
+        operator = nil
+
+        (1...report.size).all? do |i|
+          a = report[i - 1].to_i
+          b = report[i].to_i
+          diff = (a - b).abs
+          operator ||= a > b ? :> : :<
+          a.send(operator, b) && diff >= 1 && diff <= 3
+        end
+      end
+    end.tap do |n|
+      puts n
+    end
