a5e8790bd91255f3d4f60b255ba8f5eded6ae7aa
Kevin Thompson <email@kevinthompson.info>
2024-12-02 22:05:50 -0800

    refactor regex to be more flexible

--- 2024/03/part_2.rb
+++ 2024/03/part_2.rb
@@ -2,10 +2,10 @@ input = File.read(File.join(__dir__, 'input.txt'))
 enabled = true
 result = 0
 
-input.scan(/((mul|do|don't)\((\d+),(\d+)\))/).each do |(_match, function, *args)|
+input.scan(/(mul|do|don't)\(((?:,?\d+)*)\)/).each do |(function, args)|
   case function
   when 'mul'
-    result += args.map(&:to_i).reduce(:*) if enabled
+    result += args.split(',').map(&:to_i).reduce(:*) if enabled
   when 'do', "don't"
     enabled = function == 'do'
   end
