ed9c921a5fa9c6ad1ef6e5db9f97516a82b16842
Kevin Thompson <email@kevinthompson.info>
2024-12-05 20:32:40 -0800

    formatting

--- 2024/02/part_1.rb
+++ 2024/02/part_1.rb
@@ -1,15 +1,15 @@
-File.read(File.join(__dir__, 'input.txt'))
-    .split("\n")
-    .map(&:split)
-    .count do |report|
-      operator = nil
-      (1...report.size).all? do |i|
-        a = report[i - 1].to_i
-        b = report[i].to_i
-        diff = (a - b).abs
-        operator ||= a > b ? :> : :<
-        a.send(operator, b) && diff >= 1 && diff <= 3
-      end
-    end.tap do |n|
-      puts n
+input = File.read(File.join(__dir__, 'input.txt'))
+result = input.split("\n")
+  .map(&:split)
+  .count do |report|
+    operator = nil
+    (1...report.size).all? do |i|
+      a = report[i - 1].to_i
+      b = report[i].to_i
+      diff = (a - b).abs
+      operator ||= a > b ? :> : :<
+      a.send(operator, b) && diff >= 1 && diff <= 3
     end
+  end
+
+puts result
