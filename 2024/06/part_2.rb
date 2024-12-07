6a6e19821d90d7bac63be75ead45f2a1c69508f3
Kevin Thompson <email@kevinthompson.info>
2024-12-06 21:02:50 -0800

    I don't know man...

--- 2024/06/part_2.rb
+++ 2024/06/part_2.rb
@@ -1,4 +1,4 @@
-INPUT = File.read("#{__dir__}/input.txt")
+INPUT = File.read("#{__dir__}/example.txt")
 LINES = INPUT.lines
 
 WIDTH = LINES.first.gsub(/\n/,'').strip.length
@@ -13,28 +13,44 @@ def walk_grid(grid, start: START, dir: 0, visited: {}, detect_loops: false)
   loops = 0
 
   while (0...grid.length).include?(index) do
+    system "clear"
+    grid.chars.each_with_index do |char, i|
+      print "\n" if i % WIDTH == 0
+      print char
+    end
+    sleep 0.2
+
+    # detect loop
     path[index] ||= 0
     path[index] |= (1 << dir)
+
+    # get next index
     next_index = index + OFFSETS[dir]
 
+    # detect index out of bounds
     break if dir.odd? && next_index / WIDTH != index / WIDTH
     break if !(0...grid.length).include?(next_index)
 
-    if grid[next_index] == '#'
+    # detect obstruction
+    while grid[next_index] == '#'
       dir = (dir + 1) % 4
-    else
-      if detect_loops && next_index != START
-        loop_grid = grid.dup
-        loop_grid[next_index] = '#'
-        loops += walk_grid(loop_grid, start: index, visited: path, dir:)
-      end
-
-      index = next_index
+      next_index = index + OFFSETS[dir] 
     end
 
-    if path[index].to_i & (1 << dir) > 0
+    if detect_loops && next_index != START
+      loop_grid = grid.dup
+      loop_grid[next_index] = '#'
+      if walk_grid(loop_grid, start: index, visited: path, dir:) == 1
+        loops += 1
+      end
+    end
+
+    # detect loop
+    if path[next_index].to_i & (1 << dir) > 0
       return 1
     end
+
+    index = next_index
   end
 
   loops
