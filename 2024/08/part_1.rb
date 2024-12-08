e593887475f3a78f370d585308b4db85db3a6c9e
Kevin Thompson <email@kevinthompson.info>
2024-12-07 23:23:51 -0800

    bring back vectors for boundary checks

--- 2024/08/part_1.rb
+++ 2024/08/part_1.rb
@@ -1,6 +1,8 @@
+require 'matrix'
 require_relative('../../lib/support')
 
 solve example: 14 do |input|
+  width = input.lines.first.chomp.length
   grid = input.gsub(/\n/,'')
   antenna_char_indexes = {}
   antinode_indexes = Set.new
@@ -16,9 +18,15 @@ solve example: 14 do |input|
     next if indexes.size < 2
 
     indexes.permutation(2).map(&:sort).uniq.each do |(a, b)|
-      diff = b - a
-      [a - diff, b + diff].each do |index|
-        antinode_indexes << index if (0...grid.size).include?(index)
+      v1 = Vector[a % width, a / width]
+      v2 = Vector[b % width, b / width]
+      diff = v2 - v1
+
+      [v1 - diff, v2 + diff].each do |position|
+        if (0...width).include?(position[0]) && (0...input.lines.size).include?(position[1])
+          index = position[1] * width + position[0]
+          antinode_indexes << index 
+        end
       end
     end
   end
