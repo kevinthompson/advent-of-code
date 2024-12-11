7966181b1e6c424d0fa06141817bee0941d07bfd
Kevin Thompson <email@kevinthompson.info>
2024-12-11 13:45:43 -0800

    update template

--- .template/part_1.rb
+++ .template/part_1.rb
@@ -1,5 +1,17 @@
 require_relative('../../lib/support')
 
+class Solution
+  attr_reader :input
+
+  def initialize(input:)
+    @input = input
+  end
+
+  def call
+    # TODO: implement solution
+  end
+end
+
 solve example: nil do |input|
-  input
+  Solution.new(input:).call
 end
--- .template/part_2.rb
+++ .template/part_2.rb
@@ -1,5 +1,19 @@
 require_relative('../../lib/support')
 
-solve example: nil do |input|
-  input
+EXAMPLE_RESULT = nil
+
+class Solution
+  attr_reader :input
+
+  def initialize(input:)
+    @input = input
+  end
+
+  def call
+    # TODO: implement solution
+  end
+end
+
+solve example: EXAMPLE_RESULT do |input|
+  Solution.new(input:).call
 end
