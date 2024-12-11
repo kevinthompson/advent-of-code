a24bf23e84b8914041f7d47018daf50573ae3fdc
Kevin Thompson <email@kevinthompson.info>
2024-12-11 13:38:46 -0800

    day 11 part 2

--- 2024/11/part_2.rb
+++ 2024/11/part_2.rb
@@ -1,20 +1,36 @@
 require_relative('../../lib/support')
 
-solve do |input|
-  stones = input.split(' ').map(&:to_i)
-  blink_count = 25
-  blink_count.times do
-    stones = stones.flat_map do |stone|
-      if stone == 0
-        1
-      elsif stone.to_s.size.even?
-        string = stone.to_s
-        [string[0...string.size/2].to_i, string[string.size/2..-1].to_i]
-      else
-        (stone << 10) - (stone << 5) - (stone << 3)
-      end
+class Solution
+  attr_reader :input, :blink_count, :scores
+
+  def initialize(input:, blink_count: 25)
+    @input = input
+    @blink_count = blink_count
+    @scores = {}
+  end
+
+  def score(stone, blinks)
+    return 1 if blinks.zero?
+    next_blinks = blinks - 1
+
+    scores[[stone, blinks]] ||= if stone == '0'
+      score('1', next_blinks)
+    elsif stone.size.even?
+      first_half = stone[0...stone.size/2]
+      second_half = stone[stone.size/2..-1].to_i.to_s
+      score(first_half, next_blinks) + score(second_half, next_blinks)
+    else
+      score((stone.to_i * 2024).to_s, next_blinks)
     end
   end
 
-  stones.count
+  def call
+    input.split(' ').sum(0) do |stone|
+      score(stone, blink_count)
+    end
+  end
+end
+
+solve do |input|
+  Solution.new(input:, blink_count: 75).call
 end
