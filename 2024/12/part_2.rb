68b89e793aa1d240655b20df67caadcd1fbbfb61
Kevin Thompson <email@kevinthompson.info>
2024-12-14 09:43:07 -0800

    day 12 part 2

--- 2024/12/part_2.rb
+++ 2024/12/part_2.rb
@@ -2,56 +2,66 @@ require_relative('../../lib/support')
 
 EXAMPLE_RESULT = 1206
 DIRECTIONS = [[0,-1],[1,0],[0,1],[-1,0]]
+CORNER_POSITIONS = [[-1,-1],[1,-1],[1,1],[-1,1]]
 
 class Solution
-  attr_reader :grid, :input
+  attr_reader :grid
 
   def initialize(input:)
-    @input = input
-  end
-
-  def score(char, x, y)
-    directions = DIRECTIONS.dup
-    area = 1
-    perimeter = 0
-    grid[y][x] = "-#{char}"
-
-    while directions.size > 0 do
-      ox, oy = directions.pop
-      nx = x + ox
-      ny = y + oy
-      neighbor = nil
-
-      if nx.between?(0, grid[0].size - 1) && ny.between?(0,grid.size - 1)
-        neighbor = grid.dig(ny,nx)
-      end
-
-      if neighbor == char
-        next_area, next_perimeter = score(char, nx, ny)
-        area += next_area
-        perimeter += next_perimeter
-      elsif neighbor != "-#{char}"
-        perimeter += 1
-      end
-    end
-
-    [area, perimeter]
+    height = input.lines.size
+    width = input.lines.first.chomp.size
+    @grid = input.lines.map(&:chomp).map { |line| line.split('') }
   end
 
   def call
-    result = 0
+    grid.each_with_index.sum do |line, y|
+      line.each_with_index.sum do |char, x|
+        area, corners = score([x, y])
+        area * corners
+      end
+    end
+  end
 
-    @grid = input.lines.map(&:chomp).map { |line| line.split('') }
-    @grid.each_with_index do |line, y|
-      line.each_with_index do |char, x|
-        if char != nil && char[0] != '-'
-          area, perimeter = score(char, x, y)
-          result += area * perimeter
-        end
+  private
+
+  def score(position)
+    char = grid.dig(*position.reverse)
+    return [0, 0] if char.nil? || char[0] == '-'
+    
+    area = 1
+    corners = count_corners(char, position)
+    grid[position[1]][position[0]] = "-#{char}"
+
+    DIRECTIONS.dup.each do |direction|
+      new_position = position.zip(direction).map(&:sum)
+      neighbor = valid?(new_position) ? grid.dig(*new_position.reverse) : nil
+
+      if neighbor == char
+        next_area, next_corners = score(new_position)
+        area += next_area
+        corners += next_corners
       end
     end
 
-    result
+    [area, corners]
+  end
+
+  def count_corners(char, position)
+    DIRECTIONS.each_with_index.sum do |direction, index|
+      offsets = [direction, DIRECTIONS[index-1]]
+      diagonal = CORNER_POSITIONS[index]
+      match = proc { |offset| match?(char, position, offset) }
+      (offsets.none?(&match) || (offsets.all?(&match) && !match.call(diagonal))) ? 1 : 0
+    end
+  end
+
+  def match?(char, position, offset)
+    new_position = offset.zip(position).map(&:sum)
+    valid?(new_position) && [char, "-#{char}"].include?(grid.dig(*new_position.reverse))
+  end
+
+  def valid?(position)
+    position[0].between?(0, grid[0].size - 1) && position[1].between?(0, grid.size - 1)
   end
 end
 
