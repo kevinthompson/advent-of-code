7bce1f039b4b8a084b76c030c080551b87ab7ed3
Kevin Thompson <email@kevinthompson.info>
2024-12-15 14:42:31 -0800

    day 13 part 2

--- 2024/13/part_2.rb
+++ 2024/13/part_2.rb
@@ -1,6 +1,6 @@
 require_relative('../../lib/support')
 
-EXAMPLE_RESULT = 480
+EXAMPLE_RESULT = nil
 
 class Solution
   attr_reader :input
@@ -14,6 +14,9 @@ class Solution
       a, b, prize = machine.split("\n").map do |str|
         str.scan(/\d+/).map(&:to_i)
       end
+
+      prize[0] += 10000000000000
+      prize[1] += 10000000000000
 
       y = (a[0] * prize[1] - a[1] * prize[0]) / ((a[0] * b[1]) - (a[1] * b[0]))
       x = (prize[0] - b[0] * y) / a[0]
